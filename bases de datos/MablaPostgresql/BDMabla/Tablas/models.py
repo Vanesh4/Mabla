6773ab3d853113753b20c0a8890afe40ffe3e3c6
programingkjbr2004 <bejaranokaren57@gmail.com>
2023-05-30 10:10:38 -0500

    tablas postgresql karen

--- bases de datos/MablaPostgresql/BDMabla/Tablas/models.py
+++ bases de datos/MablaPostgresql/BDMabla/Tablas/models.py
@@ -15,7 +15,7 @@ class TablaUsuario(models.Model):
 class TablaPruebas(models.Model):
     idPrueba=models.AutoField(primary_key=True)
     alias=models.ForeignKey(TablaUsuario, null=True, on_delete=models.CASCADE)
-    tipoPrueba=models.PositiveBigIntegerField(verbose_name="Tipo de prueba")
+    tipoPrueba=models.PositiveSmallIntegerField(verbose_name="Tipo de prueba")
     categoria=models.TextField(max_length=30)
     fecha=models.DateTimeField(auto_now=False)
     puntaje=models.PositiveBigIntegerField(verbose_name="Puntaje")
@@ -26,16 +26,32 @@ class TablaComentarios(models.Model):
 
 #karen
 
+class TablaCategoria(models.Model):
+    IdCategoria=models.PositiveSmallIntegerField(primary_key=True, verbose_name="IdCategoria")
+    Categoria=models.TextField(max_length=30)
+
+class TablaSubcategoria(models.Model):
+    IdSubcategoria=models.PositiveSmallIntegerField(primary_key=True, verbose_name="IdSubcategoria")
+    IdCategoria=models.ForeignKey(TablaCategoria, null=False, on_delete=models.CASCADE )
+    Subcategoria=models.TextField(max_length=30)
+
+class TablaPalabra(models.Model):
+    Palabra=models.TextField(primary_key=True)
+    IdCategoria=models.ForeignKey(TablaCategoria, null=False, on_delete=models.CASCADE)
+    Senia="img"
+
 #vanessa
 class TablaPreguntas(models.Model):
-    tipo = models.TextField(int, verbose_name="Tipo")
-    idCategoria = models.ForeignKey(TablaCategorias, null False, on_delete=models.CASCADE)
-    senia = models.TextField()
-    respuesta = models.TextField()
-    numeroPregunta = models.AutoField(verbose_name="numeroPregunta")
+    tipo = models.PositiveIntegerField(verbose_name="Tipo")
+    idCategoria = models.ForeignKey(TablaCategoria, null=False, on_delete=models.CASCADE)
+    senia = "img"
+    respuesta = models.TextField(max_length=50)
+    numeroPregunta = models.AutoField(primary_key=True)
 
 
 class TablaPrueba_Pregunta(models.Model):
     idPrueba = models.ForeignKey(TablaPruebas, null=False, on_delete=models.CASCADE)
     idCategoria = models.ForeignKey(TablaPreguntas, null=False, on_delete=models.CASCADE)
-    puntaje = models.TextField(int, verbose_name="Puntaje")
+    puntaje=models.PositiveBigIntegerField(verbose_name="Puntaje")
+
+
