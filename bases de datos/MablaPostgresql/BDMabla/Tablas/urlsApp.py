ad252879c2129100420e484e8635c1c6ae685874
Sara <saritaprieto445@gmail.com>
2023-06-05 18:46:40 -0500

    crud usuario, pruebas y comentarios

--- bases de datos/MablaPostgresql/BDMabla/Tablas/urlsApp.py
+++ bases de datos/MablaPostgresql/BDMabla/Tablas/urlsApp.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from Tablas.models import *
 from .views import *
 
 urlpatterns = [
-    path('')
+    path('tablaUsuario',getTablaUser.as_view(), name='tablaUsuario'),
+    path('insertUser',insertTablaUser.as_view(), name='insertUser')
 ]
--- bases de datos/MablaPostgresql/BDMabla/Tablas/views.py
+++ bases de datos/MablaPostgresql/BDMabla/Tablas/views.py
@@ -1,37 +1,22 @@
 from typing import Any
 from django.http import JsonResponse
-from django.shortcuts import render
-<<<<<<< HEAD
-from django.views.generic import ListView, View
-from Tablas.models import*
-
-# Create your views here.
-
-#CRUD TABLA CATEGORIAS
-
-class GetTablaCategoria(View):
-    def get(self, request):
-        register=TablaCategoria
-=======
-from django.views import View
 from django.views.generic import View
 from django.http import HttpRequest, JsonResponse
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 import json
+from Tablas.models import *
 
-from BDMabla.Tablas.models import TablaUsuario
-
-class GetTablaUser(View):
+class getTablaUser(View):
     def get(self,request):
         register= TablaUsuario.objects.all().values()
         registerUser=list(register)
         return JsonResponse(registerUser, safe=False)
 
-class InsertTablaUser(View):
+class insertTablaUser(View):
     #notacion
     @method_decorator(csrf_exempt)
-    def dispatch(self, request: HttpRequest, *args: Any, **kwargs: Any):
+    def dispatch(self, request, *args: Any, **kwargs):
         return super().dispatch(request, *args, **kwargs)
     
     def post(self, request):
@@ -42,8 +27,116 @@ class InsertTablaUser(View):
         request.POST.get('telefono')
         request.POST.get('correo')
         request.POST.get('clave')
+        request.POST.get('imgPerfil')
         print("datos del cliente ",request.POST)
-        registerInsertUser1=TablaUsuario.objects.create(alias=registerInsertUser['alias'],nombre=registerInsertUser['nombre'],apellido=registerInsertUser['apellido'],telefono=registerInsertUser['telefono'],correo=registerInsertUser['correo'],clave=registerInsertUser['clave'])
+        registerInsertUser1=TablaUsuario.objects.create(alias=registerInsertUser['alias'],nombre=registerInsertUser['nombre'],apellido=registerInsertUser['apellido'],telefono=registerInsertUser['telefono'],correo=registerInsertUser['correo'],clave=registerInsertUser['clave'], imgPerfil=registerInsertUser['imgPerfil'])
         registerInsertUser1.save()
         return JsonResponse({'mensaje':'datos guardados'})
->>>>>>> 67acceb6d3762210977b2ddb929fab09ed39253e
+
+class editTablaUser(View):
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request: HttpRequest, *args: Any, **kwargs: Any):
+        return super().dispatch(request, *args, **kwargs)
+    
+    def put(self, request, pk):
+        try:
+            pKey=TablaUsuario.objects.get(pk=pk)
+        
+        except TablaUsuario.DoesNotExist:
+            return JsonResponse({'Error':'El alias ingresado no existe'})
+        data= json.loads(request.body)
+        pKey.nombre=data.get('nombre')
+        pKey.apellido=data.get('apellido')
+        pKey.telefono=data.get('telefono')
+        pKey.correo=data.get('correo')
+        pKey.clave=data.get('clave')
+        pKey.imgPerfil=data.get('imgPerfil')
+        pKey.save()
+        return JsonResponse({"Mensaje":"Datos actualizados"})
+
+class getTablaComment(View):
+    def get(self,request):
+        register= TablaComentarios.objects.all().values()
+        registerComment=list(register)
+        return JsonResponse(registerComment, safe=False)
+
+class insertComment(View):
+    #notacion
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args: Any, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+    
+    def post(self, request):
+        registerInsertComment=json.loads(request.body)
+        request.POST.get('alias')
+        request.POST.get('texto')
+        print("datos del cliente ",request.POST)
+        registerInsertComment1=TablaComentarios.objects.create(alias=registerInsertComment['alias'],texto=registerInsertComment['texto'])
+        registerInsertComment1.save()
+        return JsonResponse({'mensaje':'datos guardados'})
+
+class editComment(View):
+    #anotacion
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args: Any, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+    
+    def put(self, request, pk):
+        try: 
+            pKey = TablaComentarios.objects.get(pk=pk)
+        except TablaComentarios.DoesNotExist:
+            return JsonResponse({'Error':'El alias ingresado no existe'})
+        data = json.loads(request.body)
+        #objeto de tipo cliente (registro):
+        pKey.texto=data.get('texto')
+        pKey.save() 
+        return JsonResponse({"Mensaje":"Datos actualizados"})
+    
+class deleteComment(View):
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args: Any, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+    
+    def delete(self, request, pk):
+        try:
+            pKey = TablaComentarios.objects.get(pk=pk)
+        except TablaComentarios.DoesNotExist:
+            return JsonResponse({"Error":"El alias no existe"})
+        
+        pKey.delete()
+        return JsonResponse({"mensaje":"Datos eliminados"})
+
+class getTablaPrueba(View):
+    def get(self,request):
+        register= TablaPruebas.objects.all().values()
+        registerPrueba=list(register)
+        return JsonResponse(registerPrueba, safe=False)
+
+class InsertarPrueba(View):
+    #anotacion
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args: Any, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+    
+    def post(self, request):
+        #capturados:
+        registerInsertPrueba = json.loads(request.body)
+        #preparar la manera de enviar los datos
+        request.POST.get('idPrueba')
+        request.POST.get('alias')
+        request.POST.get('tipoPrueba')
+        request.POST.get('categoria')
+        request.POST.get('fecha')
+        request.POST.get('puntaje')
+        registerInsertPrueba1 = TablaPruebas.objects.create(idPrueba=registerInsertPrueba['idPrueba'],
+                                      alias=registerInsertPrueba['alias'],
+                                      tipoPrueba=registerInsertPrueba['tipoPrueba'],
+                                      categoria=registerInsertPrueba['categoria'],
+                                      fecha=registerInsertPrueba['fecha'],
+                                      puntaje=registerInsertPrueba['puntaje'])
+        registerInsertPrueba1.save()
+        #no es necesario pero es para que genere el aviso:
+        return JsonResponse({'mensaje':'datos guardados'})
+    
+
+        
