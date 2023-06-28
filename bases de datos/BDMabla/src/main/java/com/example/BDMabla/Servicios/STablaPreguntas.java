8c10bfbdcc1fca42e6f7d04567bb5d6d70a97f52
Sara <saritaprieto445@gmail.com>
2023-06-28 10:46:15 -0500

    metodo post tabla preguntas

--- bases de datos/BDMabla/src/main/java/com/example/BDMabla/Servicios/STablaPreguntas.java
+++ bases de datos/BDMabla/src/main/java/com/example/BDMabla/Servicios/STablaPreguntas.java
@@ -1,4 +1,4 @@
-/*package com.example.BDMabla.Servicios;
+package com.example.BDMabla.Servicios;
 
 import com.example.BDMabla.Entidad.*;
 
@@ -21,76 +21,9 @@ public class STablaPreguntas {
         this.repTablaCategorias = repTablaCategorias;
     }
 
-    public List<TablaPreguntas> getPreguntas(){
+    public List<TablaPreguntas> getPreguntas() {
 
         return repTablaPreguntas.findAll();
-    }*/
-
-    //insertar
-<<<<<<< HEAD
-<<<<<<< HEAD
-    /*public String postPreguntas(Integer idCategoria, TablaPreguntas p){
-
-        TablaPreguntas preg = new TablaPreguntas();
-
-        TablaCategorias cat = repTablaCategorias.findById(idCategoria).get();
-        if (repTablaCategorias.findById(idCategoria).isPresent()){
-            preg.setNumeroPregunta(p.getNumeroPregunta());
-            System.out.println("Entrooo o nooooo");
-            preg.setIdCategoria(cat);
-            System.out.println(cat);
-            preg.setTipo(p.getTipo());
-            preg.setSenia(p.getSenia());
-            preg.setRespuesta(p.getRespuesta());
-            repTablaPreguntas.save(preg);
-            return "se registro";
-        }
-        else return "no se registro";
-    }*/
-
-    /*
-    public String postPreguntas(Integer idCategoria, TablaPreguntas p){
-
-        TablaPreguntas preg = new TablaPreguntas();
-
-        TablaCategorias cat = repTablaCategorias.findById(idCategoria).get();
-        if (repTablaCategorias.findById(idCategoria).isPresent()){
-            preg.setNumeroPregunta(p.getNumeroPregunta());
-            preg.setRespuesta(p.getRespuesta());
-            preg.setSenia(p.getSenia());
-            preg.setTipo(p.getTipo());
-            preg.setIdCategoria(cat);
-            repTablaPreguntas.save(preg);
-            return "se registro";
-        }
-        else return "no se registro";
     }
-=======
-=======
-    public Boolean PostPreguntas(String cat, TablaPreguntas pregnueva){
-        TablaCategorias categoria = repTablaCategorias.findById(cat).get();
-        if(repTablaCategorias.findById(cat).isPresent()){
-            pregnueva.setTablaCategorias(categoria);
-            repTablaPreguntas.save(pregnueva);
-            return true;
-        }
-        else return false;
-    }
-
->>>>>>> vanessa
-    /*public Boolean postPregunta(Integer idCategoria, TablaPreguntas preg){
-        *//*TablaCategorias cat = repTablaCategorias.
-
-            TablaUsuario user= repoUsuario.findById(alias).get();
-            if(repoUsuario.findById(alias).isPresent()){
-                comment.setTablaUsuario(user);
-                repostorioComents.save(comment);
-                return "Comentario guardado";
-            }
-            else return "El alias no exist";*//*
-        return true;
-    }*/
->>>>>>> b8125aefa0e4cd39bd81054a99f0ac9d695eb797
-
 }
-*/
+
