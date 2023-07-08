4b023966e16dfde308f597ad9ca3ee2c3faa6ca4
vanesha <nva.gomo926@gmail.com>
2023-07-08 16:11:53 -0500

    get de comentarios y categoria

---
+++ bases de datos/MablaPostgresql/BDMabla/Tablas/static/js/incio.js
@@ -0,0 +1,48 @@
+$(document).ready (()=>{
+
+    header = document.getElementById("header")
+
+    $("#imgBotonAbrirMenu").on("click", ()=>{      
+        header.style.setProperty("display", "inherit")
+    })
+
+    $("#imgBotonCerrarMenu").on("click", ()=>{
+        //console.log("porfavor")
+        header.style.setProperty("display", "none")
+    })
+
+
+    //get Comentarios
+    commentsList = document.querySelector(".listComent")
+    
+    $.ajax({
+        url: "http://127.0.0.1:8000/tablaComment",
+        //url: "http://localhost:8080/users",
+        type: "GET",
+        dataType: "JSON",
+        success: function (res) {
+            console.log(res)
+            
+            for (let i = 0; i < 4; i++) {
+            
+                comentario = document.createElement("div")
+                comentario.setAttribute("id","comentario")
+
+                usuario = document.createElement("p")
+                usuario.setAttribute("id","user")
+                usuario.innerHTML = res[i].alias_id
+
+                texto = document.createElement("p")
+                texto.setAttribute("id","coment")
+                texto.innerHTML = res[i].texto
+
+                comentario.appendChild(usuario)
+                comentario.appendChild(texto)
+                commentsList.appendChild(comentario)
+            }
+        }
+    })
+})
+
+
+
---
+++ bases de datos/MablaPostgresql/BDMabla/Tablas/static/js/menu.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+    
+    titulosCat = document.querySelectorAll('h2')
+    
+    //console.log("porfis",titulosCat)
+    $.ajax({
+        url: "http://127.0.0.1:8000/getcate",
+        //url: "http://localhost:8080/getCategorias",
+        type: "GET",
+        dataType: "JSON",
+        success: function (res) {
+            console.log(res)
+            for(i = 0; i <= res.length ; i++){
+                titulosCat[i].innerHTML = res[i].Categoria 
+            }
+            
+            
+        } 
+    })
+})
