a38f7c029f9cdb2e1f85ee01e96187416b063a2d
programadorakjbr <bejaranokaren57@gmail.com>
2023-08-16 08:03:58 -0500

    springboot

--- bases de datos/BDMabla/src/main/java/com/example/BDMabla/Controlador/Clogin.java
+++ bases de datos/BDMabla/src/main/java/com/example/BDMabla/Controlador/Clogin.java
@@ -1,4 +1,3 @@
-/*
 package com.example.BDMabla.Controlador;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
@@ -19,5 +18,4 @@ public class Clogin {
     }
 }
 
-*/
 
