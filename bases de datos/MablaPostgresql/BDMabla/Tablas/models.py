c84848e10dff89ed197d72448f7f7eda1ad1ac49
Sara <saritaprieto445@gmail.com>
2023-08-16 16:34:18 -0500

    interfaz registro django

--- bases de datos/MablaPostgresql/BDMabla/Tablas/models.py
+++ bases de datos/MablaPostgresql/BDMabla/Tablas/models.py
@@ -1,19 +1,20 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-""" class TablaUsuario(models.Model):
+
+"""class TablaUsuario(models.Model):
     alias=models.TextField(primary_key=True, max_length=30)
     nombre=models.TextField(null= False)
     apellido=models.TextField(null= False)
     #telefono=models.PositiveBigIntegerField(verbose_name="Teléfono", null=True)
     #correo=models.TextField(max_length=30)
     #clave=models.TextField(max_length=30)
-    #user= models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='User', null=True, blank=True) """
+    #user= models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='User', null=True, blank=True)""" 
 
 class User(AbstractUser):
     imgPerfil=models.ImageField(default='user.png', upload_to='img/', null=True, blank=True)
     alias=models.TextField(primary_key=True, max_length=30)
-    #alias= models.OneToOneField(TablaUsuario,primary_key=True, on_delete=models.CASCADE)
+    #alias= models.OneToOneField(TablaUsuario,primary_key=True, on_delete=models.CASCADE) 
 
 class TablaComentarios(models.Model):
     alias=models.ForeignKey(User, null=True, on_delete=models.CASCADE)
