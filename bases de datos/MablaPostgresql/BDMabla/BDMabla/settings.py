baf612ff6df44114ad1a1747d79196d20da8bcf9
Sara <saritaprieto445@gmail.com>
2023-09-05 20:36:48 -0500

    auth0 forbbiden

--- bases de datos/MablaPostgresql/BDMabla/BDMabla/settings.py
+++ bases de datos/MablaPostgresql/BDMabla/BDMabla/settings.py
@@ -54,7 +54,7 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    'http://192.168.137.1',
+    'http://10.190.82.231',
 ]
 
 AUTHENTICATION_BACKENDS = (
@@ -95,11 +95,7 @@ DATABASES = {
         'HOST':'127.0.0.1',
         'DATABASE_PORT':'5432',
         'USER':'postgres',
-<<<<<<< HEAD
-        'PASSWORD':'2004',
-=======
         'PASSWORD':'0000',
->>>>>>> c9e0fe11ca1b4a634152c5e4f870d671c58d202b
 
     }
 }
