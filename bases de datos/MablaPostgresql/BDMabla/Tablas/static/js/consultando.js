b6a8f36248ae779ea108eb8c1af5677947573c73
programingkjbr2004 <bejaranokaren57@gmail.com>
2023-09-12 07:39:49 -0500

    django-diccio-consul-karen

--- bases de datos/MablaPostgresql/BDMabla/Tablas/static/js/consultando.js
+++ bases de datos/MablaPostgresql/BDMabla/Tablas/static/js/consultando.js
@@ -3,6 +3,13 @@ $(document).ready(function () {
 contenedor = document.querySelector("#list")
 const contenidoMostrado = document.getElementById('resultado');
 boton=document.getElementById('botonQuiz')
+const cajalinks=document.getElementById('section1')
+fleimg=document.getElementById('flechacaja')
+var altocaja2=document.querySelector('.caja2')
+console.log("oaaoaoaooaoaoa",altocaja2)
+
+
+
 
     
 $.ajax({
@@ -12,12 +19,6 @@ $.ajax({
     success: function (res) { 
       
         for(let x = 0; x<=res.length; x++) {
-<<<<<<< HEAD
-=======
-            //console.log("el res for primero",res)
- 
-            //console.log("hollaaaaaa",res[x])
->>>>>>> ecc2c2e6590e07a2eb9e60f691d8ea0b7357a36e
             contenedorli=document.createElement("li")
             contenedorli.setAttribute("class","list_item list_item--click")
 
@@ -42,10 +43,6 @@ $.ajax({
             subdecategorias=res[x].Subcategorias
             //console.log("hola quiero observer",subdecategorias)
             for(let s=0; s<subdecategorias.length; s++){       
-<<<<<<< HEAD
-=======
-                //console.log("subcategorias ijuemadre en s",subdecategorias)
->>>>>>> ecc2c2e6590e07a2eb9e60f691d8ea0b7357a36e
                 
                 li = document.createElement("li")
                 li.setAttribute("class","list_inside")    
@@ -53,14 +50,7 @@ $.ajax({
                 a=document.createElement("a")
                 a.setAttribute("class","nav_link nav_link--inside")
                 a.innerHTML=subdecategorias[s].subcategoria
-<<<<<<< HEAD
                
-=======
-                //console.log(subdecategorias[s].subcategoria)
-
-                //console.log("que hay en a",a)
-                
->>>>>>> ecc2c2e6590e07a2eb9e60f691d8ea0b7357a36e
                 li.appendChild(a)
                 contenedorsub.appendChild(li)
                 contenedorli.appendChild(contenedorsub)
@@ -68,28 +58,28 @@ $.ajax({
                 paldesubcate=subdecategorias[s].palabraas
                 //console.log("palabras en subcate",paldesubcate)
                 
-<<<<<<< HEAD
                     console.log("siiiiiiiiiiiiiiiiiiiiiiiii?")
-=======
-
-                    //console.log("siiiiiiiiiiiiiiiiiiiiiiiii?")
->>>>>>> ecc2c2e6590e07a2eb9e60f691d8ea0b7357a36e
                     enlaces = document.querySelectorAll('.nav_link--inside');
+
                     //console.log("capturando etiquetas a ")           
                     //console.log("enlaces",enlaces)
                     enlaces.forEach(enlace => {
                     //console.log("entra al foreach con enlace",enlaces)    
-                    enlace.addEventListener('click', function(event) {
 
+                    enlace.addEventListener('click', function(event) {
+                       
+                        
+                        cajalinks.classList.add('cajasindisplay')
+                        fleimg.id= 'flechacajadis'
+                        
+                        
+
+                     
                         event.preventDefault();
                         //const valor = enlace.getAttribute('data-valor'); 
                         valor = enlace.textContent;
                         //console.log("mirando valor",valor)
                         contenidoMostrado.textContent = ContenidoDeBaseDeDatos(valor);
-<<<<<<< HEAD
-=======
-                        //console.log("que hay en el contenedor ",ContenidoDeBaseDeDatos)
->>>>>>> ecc2c2e6590e07a2eb9e60f691d8ea0b7357a36e
                 
                     function ContenidoDeBaseDeDatos(valor) {
                         $.ajax({
@@ -97,19 +87,10 @@ $.ajax({
                             data: {valor: valor},
                             dataType: 'json',
                             success: function(data) {
-<<<<<<< HEAD
-=======
-                                //console.log("valoooooooooor"+valor)
-                                //console.log("ver lo que hay en data palabras",data)
->>>>>>> ecc2c2e6590e07a2eb9e60f691d8ea0b7357a36e
                                 var resultsDiv = $('#resultado');
                                                           
                                resultsDiv.empty();                      
                                 var palabras = data.palabras; 
-<<<<<<< HEAD
-=======
-                                //console.log("queria mirar lo que se guarda aqui",palabras)
->>>>>>> ecc2c2e6590e07a2eb9e60f691d8ea0b7357a36e
                                 palabras.forEach(function(palabra) {
                     
                                     grupopalabra=document.createElement('div')
@@ -127,40 +108,27 @@ $.ajax({
                                     grupopalabra.append(p)
                                     contenidoMostrado.append(grupopalabra)
                                     resultsDiv.append(grupopalabra)
-<<<<<<< HEAD
 
                                     //llamar el boton y mostrarlo
-=======
-  
-                                    //console.log("boooooooooooo",$("#botonQuiz").val(res[x].Categoria))
->>>>>>> ecc2c2e6590e07a2eb9e60f691d8ea0b7357a36e
                                     $('.boton').css("display","block")
+                                    $('#botonQuiz')
                                     
                                     
                                 });
                                
                             }
-<<<<<<< HEAD
                    
-=======
-
-                    
->>>>>>> ecc2c2e6590e07a2eb9e60f691d8ea0b7357a36e
                             });
                             
                     }
                                      
                 });
+               
             });
                       
             } 
-<<<<<<< HEAD
           
            contenedor.appendChild(contenedorli)
-=======
-
-            contenedor.appendChild(contenedorli)
->>>>>>> ecc2c2e6590e07a2eb9e60f691d8ea0b7357a36e
 
             let listElements=document.querySelectorAll('.list_button--click');
 
@@ -178,58 +146,46 @@ $.ajax({
                         valo=listElement.innerText
                         //console.log("veeeeeeeeeer",valo)
                         boton.setAttribute("value", valo)
-<<<<<<< HEAD
-                                                         
-=======
   
                                                           
->>>>>>> ecc2c2e6590e07a2eb9e60f691d8ea0b7357a36e
                     }
                     menu.style.height =`${height}px`;
 
                 })             
             });
-<<<<<<< HEAD
                               
         }
+
+
         
     }   
 })
 
 
-=======
-           
-        }
+
+
+fleimg.addEventListener('click', function() {
+    
+   
+    
+    console.log("que hay en caja 2",altocaja2)
+    // Verifica el estado actual de visibilidad del contenido
+    if (cajalinks.style.display === 'none' || cajalinks.style.display === '') {
+      // Si está oculto, muéstralo
+      
+      cajalinks.style.display = 'block'
+      altocaja2.classList.add('caja2alt')
+      
+      
+    } else {
+      // Si está visible, ocúltalo
+      cajalinks.style.display = 'none';
     }
->>>>>>> ecc2c2e6590e07a2eb9e60f691d8ea0b7357a36e
-})
 
 
-
-<<<<<<< HEAD
-=======
-cat = document.getElementById("botonQuiz").value
-localStorage.setItem('categoria', cat)
-
-function generarNumeroAleatorio() {
-    return Math.floor(Math.random() * 2) + 1;
-}
-console.log(generarNumeroAleatorio())
-
-function llevar() {
-    if (generarNumeroAleatorio()==1) {
-        window.location.href = "http://127.0.0.1:8000/quizTipo1";
-    }
-    else if(generarNumeroAleatorio()==2){
-        window.location.href = "http://127.0.0.1:8000/quizTipo2";
-    }
-}
-
-$("#botonQuiz").click(function() {
-    llevar()
-})
+  });
+  
 
 })
->>>>>>> ecc2c2e6590e07a2eb9e60f691d8ea0b7357a36e
 
 
